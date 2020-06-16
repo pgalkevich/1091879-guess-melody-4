9c95019dfc2f3e219697ee287a02927dd4881ddc
pgalkevich <pgalkevich@gmail.com>
2020-06-16 14:10:13 +0300

    module2-task2

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const WelcomeScreen = (props) => {
-  const {errorsCount} = props;
+  const {errorsCount, onWelcomeButtonClick} = props;
 
   return (
     <section className="welcome">
       <div className="welcome__logo">
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
       </div>
-      <button className="welcome__button">
+      <button className="welcome__button" onClick={onWelcomeButtonClick}>
         <span className="visually-hidden">Начать игру</span>
       </button>
       <h2 className="welcome__rules-title">Правила игры</h2>
@@ -24,7 +24,8 @@ const WelcomeScreen = (props) => {
 };
 
 WelcomeScreen.propTypes = {
-  errorsCount: PropTypes.number.isRequired
+  errorsCount: PropTypes.number.isRequired,
+  onWelcomeButtonClick: PropTypes.func.isRequired
 };
 
 export default WelcomeScreen;
